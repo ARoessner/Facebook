c64fbe65708d79cee03d89ca1a42249100431b51
ARoessner <And13@comcast.net>
2018-05-23 10:58:17 -0400

    Created Login Page

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  Uname : String;
+  Pword : String;
   constructor() { }
-
+  login()
+  {
+    console.log(this.Uname);
+    console.log(this.Pword);
+  }
   ngOnInit() {
   }
 
